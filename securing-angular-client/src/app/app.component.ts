c31b2137c889fe3ee6f1685e74b28aa9f237a32e
Judyll Agan <judyll.agan@outlook.com>
2020-05-23 10:03:43 +0800

    Adding login to the app.

--- securing-angular-client/src/app/app.component.ts
+++ securing-angular-client/src/app/app.component.ts
@@ -23,4 +23,13 @@ export class AppComponent implements OnInit {
       this.isLoggedIn = loggedIn;
     });
   }
+
+  /** We just need to call the authService.login method which returns a promise
+   * but there is rarely a reason to handle it because calling the signinRedirect()
+   * in the User Manager is going to result in the redirect to the STS immediately.
+   * Which means your app will be unloading from memory anyway as soon as that is called.
+   */
+  login() {
+    this._authService.login();
+  }
 }
